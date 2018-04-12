395cc62710206975d3d36d521ce67d8490fe59ab
DESKTOP-V9F23JT\Aga diMaganda <melihhann@gmail.com>
2018-04-13 02:42:48 +0300

    Berber Ekle sayfasına harita eklendi.

--- app/src/main/java/com/example/agadimaganda/findyourownbarber/AddBarberActivity.java
+++ app/src/main/java/com/example/agadimaganda/findyourownbarber/AddBarberActivity.java
@@ -1,67 +1,89 @@
 package com.example.agadimaganda.findyourownbarber;
 
 import android.content.Intent;
-import android.os.Build;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.firebase.client.Firebase;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.apache.commons.math3.analysis.function.Add;
+import java.io.IOException;
+import java.util.Locale;
 
-import static java.lang.StrictMath.toIntExact;
-
-public class AddBarberActivity extends AppCompatActivity {
+public class AddBarberActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     //Database
     private DatabaseReference mRef;
 
     //User Interface
     private EditText barberNameEditText;
-    private EditText latitudeEditText;
-    private EditText longitudeEditText;
-    private EditText cityEditText;
     private Button button;
 
+    //Map
+    private MapView mapView;
+    private GoogleMap map;
+    Geocoder geocoder;
+    java.util.List<Address> address = null;
+    Marker marker;
 
     //Classes
-    private Barber newBarber;
+    private Barber newBarber = new Barber();
     //private coolMethods coolMethods = new coolMethods();
 
     //Variables
     private int currentId;
     private Boolean flag = false;
+    private BitmapDescriptorFactory bitmapDescriptorFactory;
+    private Boolean isMarkerAdded = false;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_barber);
 
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        mapView.getMapAsync(this);//
+
         button =  findViewById(R.id.saveButtonAction);
         barberNameEditText = findViewById(R.id.barberName_editText);
-        latitudeEditText = findViewById(R.id.latitude_editText);
-        longitudeEditText = findViewById(R.id.longitude_editText);
-        cityEditText = findViewById(R.id.city_editText);
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                newBarber = new Barber();
 
                 newBarber.setBarberName(barberNameEditText.getText().toString());
-                newBarber.setLatitude(Double.valueOf(latitudeEditText.getText().toString()));
-                newBarber.setLongitude(Double.valueOf(longitudeEditText.getText().toString()));
-                newBarber.setCity((cityEditText.getText().toString()));
+                //newBarber.setLatitude(Double.valueOf(latitudeEditText.getText().toString()));
+                //newBarber.setLongitude(Double.valueOf(longitudeEditText.getText().toString()));
+                //newBarber.setCity((cityEditText.getText().toString()));
 
                     mRef = FirebaseDatabase.getInstance().getReference();
                     final DatabaseReference childRef = mRef.child("BARBERS").child(newBarber.getBarberName().toUpperCase().replace(" ", ""));
@@ -98,19 +120,74 @@ public class AddBarberActivity extends AppCompatActivity {
                     childRef.child("LONGITUDE").setValue(newBarber.getLongitude());
                     childRef.child("CITY").setValue(newBarber.getCity());
 
-                    /*
-                    Bundle bundle = new Bundle();
-                    bundle.putDouble("latitude",newBarber.getLatitude());
-                    bundle.putDouble("longitude",newBarber.getLongitude());
-                    bundle.putInt("id",newBarber.getId());
-                    bundle.putString("barberName",newBarber.getBarberName());
-                    Intent intent1 = new Intent(AddBarberActivity.this, MapsActivity.class);
-                    intent1.putExtras(bundle);
-                    startActivity(intent1);
-                    */
+
+                    marker.remove();
+                    barberNameEditText.setText(""); 
                 }
 
         });
     }
 
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        map = googleMap;
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+        }
+
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+
+        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+
+                if(isMarkerAdded){
+                    marker.remove();
+                    isMarkerAdded = false;
+                }
+
+                LatLng newBarberMarker = new LatLng(latLng.latitude, latLng.longitude);
+                 marker = map.addMarker(new MarkerOptions()
+                        .position(newBarberMarker).icon(bitmapDescriptorFactory.fromResource(R.drawable.makasufakbuyukufak)));
+
+                isMarkerAdded = true;
+                newBarber.setLatitude(latLng.latitude);
+                newBarber.setLongitude(latLng.longitude);
+
+                try{
+                    address = geocoder.getFromLocation(newBarber.getLatitude(), newBarber.getLongitude(), 1);
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+
+                if(address != null){
+                    if(address.get(0).getAdminArea() != null){
+                        newBarber.setCity(address.get(0).getAdminArea());
+                    }
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 }
